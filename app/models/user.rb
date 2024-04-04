8b9165b59c44ec55bd48a094cf3ae5558bc7f713
“MatsumuraSaki” <massaki3156@gmail.com>
2024-04-04 17:50:06 +0900

    受講期にバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :target, inclusion: {in: [true, false]}
-
+  validates :generation, numericality: {only_integer: true}, length: { maximum: 4 }, allow_blank: true
   # ソート
   scope :target, -> { order(target: :desc) }
   scope :generation, -> { order(generation: :desc) }
